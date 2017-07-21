9fc45e61568d4ad858485b43af028a86b1911115
Abdul Rauf <abdulraufmujahid@gmail.com>
2017-07-21 23:22:11 +0500

    Committing auth/auth0-variables.js for deployment via github

---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: 'qT38YFKNYwj_4b9TcubohvUhAwjMr-1g',
+  domain: 'armujahid.auth0.com',
+  callbackUrl: 'http://localhost:8080/callback',
+  apiUrl: 'API_IDENTIFIER'
+}
